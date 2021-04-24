5805b55728541c815ee1283eafb4f2f514bd179a
Tsantaella <tonisantaella20122000@gmail.com>
2021-04-24 19:12:12 +0200

    Escena básica y pequeño script para la simulación de la pelota

---
+++ simulation/Assets/scripts/ScriptBall.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptBall : MonoBehaviour
+{
+    Rigidbody rigidbody;
+    public float force = 20f;
+    float time = 0f;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = time;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+    }
+
+
+    void OnMouseOver()
+    {
+        if(timer < 0)
+        {
+            Vector3 vect = new Vector3(0, 0, -10);
+
+            rigidbody.AddForce(vect * force);
+            time = 5.0f;
+            timer = time;
+        }
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+
+
+            
+            
+        //}
+    }
+}
