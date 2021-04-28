8e2f7f8d78e714a9b128f48a9152bf0747f32dbe
gerard_08 <gerardmaresp@gmail.com>
2021-04-28 18:36:55 +0200

    server funcional para recibir

--- server.py
+++ server.py
@@ -4,9 +4,10 @@ import numpy as np
 
 HOST = '127.0.0.1'
 PORT = 65430
-a = False
 
-while True:
+toni = []
+i = 0
+while i < 10:
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind((HOST, PORT))
@@ -15,14 +16,27 @@ while True:
         print("listening...")
         conn, addr = s.accept()
         print('Connected by', addr)
-        data = conn.recv(65507)
-        while data:
-            print(cv2.imdecode(np.frombuffer(data, np.uint8), -1))
-            data = conn.recv(65507)
-
-        sendData = "Mersi :)"
-        conn.send(sendData.encode())
+        data = conn.recv(786432)
+        #while data:
+        a = cv2.imdecode(np.frombuffer(data, np.uint8), -1)
+        print(len(toni))
+        toni.append(a)
+        #data = conn.recv(786432)
+        #cv2.imshow('nombre', a)
+        #cv2.waitKey(0)
+        #sendData = "Mersi :)"
+        #conn.send(sendData.encode())
         conn.close
+        del data
+        i = i + 1
     except KeyboardInterrupt:
         exit()
 
+cv2.imshow('nombre', toni[0])
+cv2.waitKey(0)
+cv2.imshow('nombre', toni[2])
+cv2.waitKey(0)
+cv2.imshow('nombre', toni[5])
+cv2.waitKey(0)
+cv2.imshow('nombre', toni[9])
+cv2.waitKey(0)
