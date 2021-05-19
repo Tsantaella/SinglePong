6382a7c4167cff10d147fcb49a3cd7f017d23309
gerard_08 <gerardmaresp@gmail.com>
2021-05-19 13:04:33 +0200

    codigo limpio

--- ballDetection.py
+++ ballDetection.py
@@ -5,22 +5,12 @@ RLP
 """
 import cv2
 import numpy as np
- 
-#Iniciamos la camara
-# camara = cv2.VideoCapture(0)
-# activada = True
-# while(activada):
-
-#     #Capturamos una imagen y la convertimos de RGB -> HSV
-#     ret, imagen = camara.read()
-
-#     if (ret):
 
 def detectBall(imagen):
     hsv = cv2.cvtColor(imagen, cv2.COLOR_BGR2HSV)
     h,w,m=imagen.shape
     
-    #En este caso un tapon rojo
+    #En este caso un balon rojo
     bajos = np.array([0,100,100], dtype=np.uint8)
     altos = np.array([20,255,255], dtype=np.uint8)
     
@@ -31,8 +21,7 @@ def detectBall(imagen):
     moments = cv2.moments(mask)
     
     area =int(moments['m00'])
-    #Descomentar para ver el area por pantalla, iniciamos valores de coordenadas a '0'
-    #print(area)
+    #Iniciamos valores de coordenadas a '0'
     x=0
     y=0
     dist=0
@@ -44,29 +33,9 @@ def detectBall(imagen):
         y = float(moments['m01']/moments['m00'])
 
         #Mostramos sus coordenadas respecto al eje           
-        #print ("Coordenadas, Area: ", (x-w/2),(-(y-h/2)),float(area))
         x = str("%.2f" %(x-w/2))
         y = str("%.2f" %(-(y-h/2)))
         #print(area)
         return (x,y,area)
-        #Rectangulo blanco en medio del objeto
-        #cv2.rectangle(imagen, (x-5, y-5), (x+5, y+5),(255,255,255), 18)
-        
-        #Ejes vertical y perpendicular al objeto
     else:
         return str(-1)
-    # cv2.line(imagen,(0,y),(w,y),(0,0,0),2)        
-    # cv2.line(imagen,(x,0),(x,h),(0,0,0),2) 
-
-    # #Divide la imagen en cuatro cuadrantes
-    # cv2.line(imagen,(0,int(h/2)),(w,int(h/2)),(255,255,255),2)        
-    # cv2.line(imagen,(int(w/2),0),(int(w/2),h),(255,255,255),2)                   
-
-    #Mostramos la imagen original con la marca del centro y la mascara
-    # cv2.imshow('mask', mask)
-    # cv2.imshow('Camara', imagen)
-    # tecla = cv2.waitKey(5) & 0xFF
-    # if tecla == ord('q'):
-        
-    #     break 
-# cv2.destroyAllWindows()
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import cv2
 import numpy as np
-from ballDetection import *
+from ballDetection import detectBall
 
 HOST = '127.0.0.1'
 PORT = 65430
@@ -13,29 +13,19 @@ while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         #le hacemos bind al puerto y host
         s.bind((HOST, PORT))
-        #print("binded...")
         #esperamos a escuchar una conexión
         s.listen()
-        #print("listening...")
         #aceptamos cuando tengamos una conexión
         conn, addr = s.accept()
-        #print('Connected by', addr)
+
         #recibimos la imagen
         data = conn.recv(786432)
-        #conn.close
-
         #la pasamos de bytes a numeros
         img = cv2.imdecode(np.frombuffer(data, np.uint8), -1)
-        #cv2.imshow('nombre', a)
-        #cv2.waitKey(0)
 
-        #creamos el socket
-        #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        #le hacemos bind al puerto y host
-        #s.connect((HOST, PORT))
-
+        #creamos datos a enviar
         sendData = str(detectBall(img))
-        #sendData = "Mersi :)"
+        #los enviamos al socket
         conn.send(sendData.encode())
 
         #cerramos conexión del socket
